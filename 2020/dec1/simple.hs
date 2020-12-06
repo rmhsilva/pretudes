7f3712f33b9dbe4c4a13efea5c4b3b0695c1b55c
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-12-06 17:39:16 +0000

    Clean up a little

--- 2020/dec1/simple.hs
+++ 2020/dec1/simple.hs
@@ -3,5 +3,11 @@
 main = do
   dat <- readFile "data.txt"
   let xs = [ read x :: Int | x <- lines dat ]
+
   print $ head [ a * b | a <- xs, b <- xs, a + b == 2020]
-  print $ head [ a * b * c | a <- xs, b <- xs, c <- xs, a + b + c == 2020]
+
+  print $ head [ a * b * c
+               | a <- xs,
+                 b <- xs,
+                 c <- xs,
+                 a + b + c == 2020]
