334e9caeb699d4ded3582b0c2a9fc9a67edbc667
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-12-14 22:43:07 +0000

    Dec14 done

--- 2020/dec14/Dec14.hs
+++ 2020/dec14/Dec14.hs
@@ -1,19 +1,24 @@
 -- Bits! Use Integer to ensure the full range, but since I'm on a 64 bit machine
 -- I could also use Int (probably).
+--
+-- Lesson: be careful with bits and off-by-ones.
 
 import Data.List.Split (splitOn)
 import Data.Bits (( .|. ), ( .&. ), complement)
 
+import Data.List  (elemIndices)
 import Data.Char  (digitToInt)
 import Data.Maybe (listToMaybe, fromJust)
 import Numeric    (readInt)
+
+import Control.Monad (filterM)
 
 import qualified Data.Map as Map
 
 
 data Instruction =
   Mem { addr :: Integer, value :: Integer } |
-  Msk { and_mask :: Integer, or_mask :: Integer }
+  Msk { zeros :: Integer, ones :: Integer, floats :: [Int] }
   deriving Show
 
 data VM = VM { memory :: Map.Map Integer Integer,
@@ -21,19 +26,43 @@ data VM = VM { memory :: Map.Map Integer Integer,
   deriving Show
 
 
-initvm = VM Map.empty (Msk (-1) 0)
+initvm = VM Map.empty (Msk (-1) 0 [])
+
+
+-- PART 1 weird mask: the Xs are don't care.
+-- The 0s change to zeros. This is an AND mask.
+-- The 1s change to ones. This is an OR mask.
+--
+-- So it's a double mask: AND where the inverse of where the mask was 0s.
+--                        OR  where the mask was 1s.
 
 execute :: VM -> Instruction -> VM
-execute vm mask@(Msk _ _) = vm { bitmask = mask }
-execute vm (Mem addr val) = vm { memory = Map.alter newval addr (memory vm) }
+execute vm mask@Msk {} = vm { bitmask = mask }
+execute vm (Mem addr val)   = vm { memory = Map.alter newval addr (memory vm) }
   where
-    newval _ = Just $ (val .&. and_mask (bitmask vm)) .|. or_mask (bitmask vm)
+    newval _ = Just $ (val .&. complement (zeros (bitmask vm))) .|. ones (bitmask vm)
 
 
-run :: [Instruction] -> VM
-run = foldl execute initvm
 
-solve1 = sum . Map.elems . memory . run
+execute2 :: VM -> Instruction -> VM
+execute2 vm mask@Msk {} = vm { bitmask = mask }
+execute2 vm (Mem addr val) = vm { memory = Map.union newmem (memory vm) }
+  -- Map.union favours the first arg in conflicts
+  where
+    newmem = Map.fromList $ zip addresses (repeat val)
+    positions = floats (bitmask vm)
+    powers = (2^) <$> positions
+    addr' = (addr .&. complement (sum powers)) .|. ones (bitmask vm)
+    addresses = (+addr') . sum <$> filterM (const [True, False]) powers
+
+
+run :: (VM -> Instruction -> VM) -> [Instruction] -> VM
+run execFn = foldl execFn initvm
+
+solve fn = sum . Map.elems . memory . run fn
+
+solve1 = solve execute
+solve2 = solve execute2
 
 ---
 
@@ -51,21 +80,12 @@ parseMem instruction arg = Mem addr val
 
 
 parseMask :: String -> String -> Instruction
-parseMask instruction arg = Msk (complement (readBin and_str)) (readBin or_str)
+parseMask instruction arg = Msk (readBin zeros_str) (readBin ones_str) floats
   where
-    and_str = map (\x -> if x == '0' then '1' else '0') arg
-    or_str = map (\x -> if x == '1' then '1' else '0') arg
+    zeros_str = map (\x -> if x == '0' then '1' else '0') arg
+    ones_str = map (\x -> if x == '1' then '1' else '0') arg
+    floats = (length arg - 1 -) <$> elemIndices 'X' arg
 
-
--- converting binary to decimal: replace all the chars I don't care about, then
--- readBinary.
-
--- weird mask: the Xs are don't care.
--- The 0s change to zeros. This is an AND mask.
--- The 1s change to ones. This is an OR mask.
---
--- So it's a double mask: AND where the inverse of where the mask was 0s.
---                        OR  where the mask was 1s.
 
 parse :: String -> Instruction
 parse line =
@@ -77,14 +97,24 @@ parse line =
       _     -> parseMask
 
 
-
+td :: [Instruction]
 td = map parse ["mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
                 "mem[8] = 11",
                 "mem[7] = 101",
-                "mem[8] = 0"]
+                "mem[8] = 0"] -- > 165
+
+td2 :: [Instruction]
+td2 = map parse ["mask = 000000000000000000000000000000X1001X",
+                 "mem[42] = 100",
+                 "mask = 00000000000000000000000000000000X0XX",
+                 "mem[26] = 1"] -- > 208
 
 main = do
   dat <- readFile "data.txt"
   let input = map parse $ lines dat
 
   print $ solve1 input
+  -- 15514035145260
+
+  print $ solve2 input
+  -- 3926790061594
