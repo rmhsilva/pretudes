efc91d6706dd8ddf1c9d2f665a87fda3dd510024
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-12-15 20:53:45 +0000

    Part 2 done

--- 2020/dec15/Dec15.hs
+++ 2020/dec15/Dec15.hs
@@ -2,22 +2,61 @@
 --
 -- Turns out (//) is O(n) where n is the size of the array.
 -- https://stackoverflow.com/questions/7289314/how-fast-is-data-array
+--
+-- Data.Map won't help us here either. Haskell is NOT the right language for
+-- today's task. ST is *disgusting*.
 
 module Dec15 where
 
 import Data.Array
+import Control.Monad (forM_)
+import Control.Monad.ST (ST, runST)
+import Data.STRef (newSTRef, writeSTRef, readSTRef)
+import Data.Array.ST (STUArray)
+import Data.Array.MArray (MArray, newArray, readArray, writeArray)
 
+
+-- PART 2
+
+-- Use two mutable arrays to track if and where numbers have been seen
+
+solve2 :: Int -> [Int] -> Int
+solve2 size xs = runST $ do
+  seen_before <- newArray (0, size-1) False :: ST s (STUArray s Int Bool)
+  seen_at     <- newArray (0, size-1) 0     :: ST s (STUArray s Int Int)
+
+  forM_ (zip xs [0..]) $ \(x,i) -> do
+    writeArray seen_before x True
+    writeArray seen_at x i
+
+  last_spoken <- newSTRef (last xs)
+
+  forM_ [(length xs) .. size - 1] $ \turn -> do
+    last <- readSTRef last_spoken
+    seen <- readArray seen_before last
+    at   <- readArray seen_at last
+
+    let current_number = if not seen then 0 else turn - 1 - at
+
+    writeSTRef last_spoken current_number
+    writeArray seen_at last (turn - 1)
+    writeArray seen_before last True
+
+  readSTRef last_spoken
+
+
+--- PART 1
 
 -- make an array, N items long, storing the 'turn number' of the last time that
 -- index was spoken.
 
-data GameState = GameState { memory :: Array Integer (Maybe Integer),
-                             turn :: Integer,
-                             last_spoken :: Integer }
+data GameState = GameState { memory :: Array Int (Maybe Int),
+                             turn :: Int,
+                             last_spoken :: Int }
   deriving Show
 
-initGame :: Integer -> [Integer] -> GameState
-initGame size xs = GameState mem (toInteger $ length xs) (last xs)
+initGame :: Int -> [Int] -> GameState
+initGame size xs = GameState mem (length xs) (last xs)
   where
     mem = listArray (0, size-1) (repeat Nothing) // zip xs (Just <$> [0..])
 
@@ -32,21 +71,16 @@ step game@(GameState memory turn last_spoken) =
       Nothing -> 0
       Just x -> turn - 1 - x
 
-solve :: Integer -> [Integer] -> Integer
+
+solve :: Int -> [Int] -> Int
 solve n xs = last_spoken $ until ((==n) . turn) step (initGame n xs)
 
 
 ---
 
-tests :: [([Integer], Integer, Integer)]
-tests = [([0,3,6], 2020, 436),
-         ([0,3,6], 30000000, 175594)]
+td = [0,3,6] :: [Int] -- 2020 -> 436, 30000000 -> 175594
 
-test = map (\(input, count, result) -> solve count input == result) tests
-
-td = [0,3,6] -- 2020 -> 436
-
-input = [1,12,0,20,8,16]
+input = [1,12,0,20,8,16] :: [Int]
 
 
 main = do
@@ -54,4 +88,5 @@ main = do
   print $ solve 2020 input
   -- 273
 
-  -- print $ solve 30000000 input
+  print $ solve2 30000000 input
+  -- 47205
