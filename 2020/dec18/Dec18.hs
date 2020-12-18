b27f8358589aba81d01166107e72c171862d93c4
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2020-12-18 23:33:49 +0000

    Neaten up dec 18

--- 2020/dec18/Dec18.hs
+++ 2020/dec18/Dec18.hs
@@ -18,39 +18,43 @@ natural    = P.natural lexer
 reservedOp = P.reservedOp lexer
 
 
--- expr     = buildExpressionParser table expr
-
-expr1    = buildExpressionParser table1 (term expr1)
-        <?> "expression"
-
-table1   = [ [binary "+" (+) AssocLeft, binary "*" (*) AssocLeft ] ]
-
-binary  name fun = Infix (do { reservedOp name; return fun })
-
+-- terms in our expression are simple
 
 term e  =  parens e
         <|> natural
         <?> "simple expression"
 
 
+-- two tables, for part 1 and 2
+
+table1  = [ [binary "+" (+) AssocLeft, binary "*" (*) AssocLeft ] ]
+
+table2  = [ [binary "+" (+) AssocLeft],
+            [binary "*" (*) AssocLeft] ]
+
+
+-- and shared binary op implementation. We eval as we parse ;)
+binary name fun = Infix (do { reservedOp name; return fun })
+
+
+-- two kinds of expression parser
+
+expr1   = buildExpressionParser table1 (term expr1)
+        <?> "part 1 expression"
+
 expr2   = buildExpressionParser table2 (term expr2)
-        <?> "expression"
-
-
-table2   = [ [binary "+" (+) AssocLeft],
-             [binary "*" (*) AssocLeft] ]
-
+        <?> "part 2 expression"
 
 
 --
 
-evalString expr str = case parse expr "" str of
+parseString expr str = case parse expr "" str of
   Left e  -> error $ show e
   Right r -> r
 
 
-solve1 = sum . map (evalString expr1)
-solve2 = sum . map (evalString expr2)
+solve1 = sum . map (parseString expr1)
+solve2 = sum . map (parseString expr2)
 
 
 main = do
